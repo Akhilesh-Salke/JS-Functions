ff57a8fa53294f1b3975c54f2e608408501243c4
Akhilesh-Salke <akhileshsalke7@gmail.com>
2026-06-30 09:25:54 +0530

    JS-Objects commit

---
+++ Objects/nested-objs.js
@@ -0,0 +1,12 @@
+var car = {
+    company: "Tata",
+    model: "Nexon",
+    color: "Red",
+
+    price : {
+        value: 1000000,
+        currency: "INR"
+    }
+};
+
+console.log(car.price.value);
---
+++ Objects/object-function.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "Raj",
+    age: 20,
+    greet: function() {
+        console.log("Hello there .. I am function inside object");
+    }
+};
+
+console.log(person);
